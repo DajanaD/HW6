8fb99a20fd02c257466109b2a128b24c15c04a45
DajanaD <ccv080480@gmail.com>
2024-03-07 15:50:13 +0200

    problem 10query

--- flask-docker-app/query_10.sql
+++ flask-docker-app/query_10.sql
@@ -1,8 +1,8 @@
 --Знайти середній бал у групах з певного предмета
 SELECT g.group_name, AVG(CAST(g.grade AS FLOAT)) AS average_grade
 FROM students s
-JOIN grades g ON s.id = g.student_id
-JOIN groups g ON s.group_id = g.id
+JOIN grades g ON s.grades_id = g.student_id
+JOIN groups g ON s.group_id = g.student_id
 JOIN subjects sub ON g.subject_id = sub.id
 WHERE sub.subject_name = 'Armed forces operational officer'
 GROUP BY g.id;
